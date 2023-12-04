5b5b3f42cc7a7af73fa35ae4e58b41f1e2d9fb8b
salman-aip <avenirsalman@gmail.com>
2023-12-04 14:19:01 +0600

    added container, pageIntro and fadeIn components using clx, framer-motion packages

--- src/components/blogs.tsx
+++ src/components/blogs.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from "next";
+import { client } from "@/lib/contentful";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description:
+    "Stay up-to-date with the latest industry news as our marketing teams finds new ways to re-purpose old CSS tricks articles.",
+};
+
 export default async function BlogsPage() {
-  return (
-    <>
-    
-    </>
-  )
+
+
+  return <></>;
 }
