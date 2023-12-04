cefe71f4a1cfc69808c3b3deb4e2d8dc62d966f0
salman-aip <avenirsalman@gmail.com>
2023-12-04 14:29:47 +0600

    added list of blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BlogsPage from "@/components/blogs";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>;
+  return (
+    <div className="mb-24">
+      <BlogsPage />
+    </div>
+  );
 }
